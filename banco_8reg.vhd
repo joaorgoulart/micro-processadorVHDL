0aed920ecec99944d4d362875a54162913610db4
João R. Goulart <joaorgoulartt@gmail.com>
2023-04-13 15:24:46 -0300

    Corrigindo os pinos de entrada e saída.

--- banco_8reg.vhd
+++ banco_8reg.vhd
@@ -3,15 +3,15 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity banco_8reg is
-	port(reg1_in 	: in unsigned(15 downto 0);
-		 reg2_out	: in unsigned(15 downto 0);
-		 data_input	: in unsigned(15 downto 0);
-		 selec_reg	: in std_logic;
-		 write_en	: in std_logic;
-		 clock		: in std_logic;
-		 reset		: in std_logic;
-		 reg1_in	: out unsigned(15 downto 0);
-		 reg2_out	: out unsigned(15 downto 0);
+	port(data_input		: in unsigned(15 downto 0);
+		 selec_regA		: in unsigned(2 downto 0);
+		 selec_regB		: in unsigned(2 downto 0);
+		 selec_regWrite : in unsigned(2 downto 0);
+		 regA_out		: out unsigned(15 downto 0);
+		 regB_out		: out unsigned(15 downto 0);
+		 write_en		: in std_logic;
+		 clock			: in std_logic;
+		 reset			: in std_logic
 	)
 end entity;
 
