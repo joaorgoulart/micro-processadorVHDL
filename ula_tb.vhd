d2cc1b3a4e3a09d41e1c49081f6047fd3234031a
João R. Goulart <joaorgoulartt@gmail.com>
2023-04-14 19:07:57 -0300

    Alterando nome dos pinos de entrada (0, 1 -> A, B)

--- ula_tb.vhd
+++ ula_tb.vhd
@@ -8,72 +8,72 @@ end;
 architecture a_ula_tb of ula_tb is
 	component ula
 		port(
-			in0 : in unsigned(15 downto 0);
-			in1 : in unsigned(15 downto 0);
+			inA : in unsigned(15 downto 0);
+			inB : in unsigned(15 downto 0);
 			out0 : out unsigned(15 downto 0);
 			selec_op : in unsigned(1 downto 0)
 		);
 	end component;
-	signal in0, in1, out0 : unsigned(15 downto 0);
+	signal inA, inB, out0 : unsigned(15 downto 0);
 	signal selec_op : unsigned(1 downto 0);
 	signal sum, subt, multip : unsigned(15 downto 0);
 	signal maior : std_logic;
     begin
-		uut: ula port map(in0 => in0,
-						  in1 => in1,
+		uut: ula port map(inA => inA,
+						  inB => inB,
 						  out0 => out0,
 						  selec_op => selec_op
 						 );
 	process
 	begin
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "00";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "01";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "10";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "11";
 		wait for 50 ns;
 		
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "00";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "01";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "10";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "11";
 		wait for 50 ns;
 		
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "00";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "01";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "10";
 		wait for 50 ns;
-		in0 <= "0000000000000000";
-		in1 <= "0000000000000000";
+		inA <= "0000000000000000";
+		inB <= "0000000000000000";
 		selec_op <= "11";
 		wait for 50 ns;'
 		wait;
