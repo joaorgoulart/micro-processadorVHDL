0006e30676fffdb99e5e99aa4c520358529b96cc
João R. Goulart <joaorgoulartt@gmail.com>
2023-05-25 14:41:00 -0300

    Adding compile script

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ghdl -a ula.vhd
+ghdl -a control_unit.vhd
+ghdl -a D_ff.vhd
+ghdl -a mux.vhd
+ghdl -a uProc.vhd
+ghdl -a state_mach.vhd
+ghdl -a rom.vhd
+ghdl -a reg16bits.vhd
+ghdl -a PC.vhd
+ghdl -a mux4x1.vhd
+ghdl -a banco_8reg.vhd 
+ghdl -a testbenches/uProc_tb.vhd
+
+ghdl -r uProc_tb --wave=uProc_tb.ghw
+gtkwave uProc_tb.ghw
+
